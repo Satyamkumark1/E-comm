5b2ba64360a14feb0f22ce18180201c3a15c011e
Satyam <satyamnagar26@gmail.com>
2025-08-14 00:39:22 +0530

    Adding Vaild annotation and

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +19,22 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+
+    //Getting all  categories
     @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<List<Category>> getAllCategories(){
         List<Category> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
+    //Creating  Category
     @RequestMapping(value = "/public/categories", method = RequestMethod.POST)
-    public ResponseEntity<String> createCategory(@RequestBody Category category){
+    public ResponseEntity<String> createCategory( @Valid @RequestBody Category category){
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
 
+    //Getting CategoryById
 
     @GetMapping("/public/categories/{categoryId}")
     public ResponseEntity<Category> searchById(@PathVariable Long categoryId){
@@ -38,6 +43,7 @@ public class CategoryController {
 
     }
 
+    //Updating Category By Id
 
     @PutMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category , @PathVariable Long categoryId){
@@ -51,6 +57,8 @@ public class CategoryController {
     }
 
 
+
+    //Deleting Category By Id
 
 
     @DeleteMapping("/admin/categories/{categoryId}")
