fd2af6fc0444a4893590ff915a69d5fa629d994e
Satyam <satyamnagar26@gmail.com>
2025-08-22 19:08:33 +0530

    Refactor user management: remove UserController, UserService, and UserRepository; add Address, AppRole, and Role models for enhanced user role and address management

--- src/main/java/com/ecommerce/project/repositery/UserRepositery.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.project.repositery;
-
-import com.ecommerce.project.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositery  extends JpaRepository<User ,Long> {
-}
--- src/main/java/com/ecommerce/project/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.project.service;
-
-import com.ecommerce.project.model.User;
-import java.util.List;
-
-public interface UserService {
-    List<User> getAllUsers();
-    User createUser(User user);
-}
